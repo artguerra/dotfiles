68a4ba371cf67802b22cf7eb2ddb64f57fc57163
Arthur Guerra <conta.arthurguerra@gmail.com>
2025-09-14 21:38:29 +0200

    wrapping & update

--- nvim/lua/config/core/options.lua
+++ nvim/lua/config/core/options.lua
@@ -16,6 +16,9 @@ opt.autoindent = true -- copy indent from current line when starting new one
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
+-- wrapping (movement)
+vim.opt.whichwrap:append("<,>,[,]")
+
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
